4db824eae3081b61399e5a8760a86221c7611bc2
Yuto Inayoshi <yuuto1744@icloud.com>
2025-10-20 22:43:00 +0900

    セッション管理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_16_065635) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_20_064059) do
   create_table "application_statuses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_16_065635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "overtime_reason"
+    t.string "work_location"
     t.index ["application_status_id"], name: "index_applications_on_application_status_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
