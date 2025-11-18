a4806d388a5c425d47b094df05635ec00515b109
kikiririkakabubuoobabakeke <s2kt2s123456789@gmail.com>
2025-11-18 09:59:04 +0900

    korekososaigodatoomou

--- app/controllers/api/v1/approvals_controller.rb
+++ app/controllers/api/v1/approvals_controller.rb
@@ -5,34 +5,37 @@ class Api::V1::ApprovalsController < ApplicationController
     authorize :approval, :index?
 
     subordinate_ids = current_api_v1_user.subordinates.pluck(:id)
-
     
     applications = Application.includes(:user, :application_status)
                               .where(user_id: subordinate_ids, application_status_id: 1)
-                              .order(date: :desc) 
+                              .order(date: :desc)
 
-   
-    this_week = Time.current.beginning_of_week(:monday)..Time.current.end_of_week(:monday)
+    approval_data = Approval.joins(:application)
+                            .where(applications: { user_id: subordinate_ids })
+                            .pluck('applications.user_id', 'applications.date')
+
+    weekly_approval_counts = approval_data.each_with_object(Hash.new(0)) do |(user_id, date), counts|
+      week_start = date.beginning_of_week(:monday)
+      counts[[user_id, week_start]] += 1
+    end
 
   
-    weekly_approval_counts = Approval
-                              .joins(:application)
-                              .where(created_at: this_week)
-                              .group('applications.user_id')
-                              .count
-
 
     data = applications.map do |app|
+
+      app_week_start = app.date.beginning_of_week(:monday)
       
-      user_weekly_count = weekly_approval_counts[app.user_id].to_i
+      
+      user_weekly_count = weekly_approval_counts[[app.user_id, app_week_start]].to_i
+
       {
         id: app.id,
         date: app.date,
         reason: app.reason,
-        start_time:app.start_time,
-        end_time:app.end_time,
-        is_special_case:app.is_special_case,
-        special_reason:app.special_reason,
+        start_time: app.start_time,
+        end_time: app.end_time,
+        is_special_case: app.is_special_case,
+        special_reason: app.special_reason,
         user: {
           name: app.user.name
         },
@@ -40,7 +43,6 @@ class Api::V1::ApprovalsController < ApplicationController
           id: app.application_status.id,
           name: app.application_status.name
         },
-        
         user_weekly_approval_count: user_weekly_count
       }
     end
